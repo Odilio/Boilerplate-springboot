f3ea609d3ff2b35a543b1c076603fac91733d622
odilio <odilionoronha@gmail.com>
2022-12-13 17:39:37 -0300

    remove class

--- src/main/java/com/bootstrap/springboot/util/Converter.java
+++ src/main/java/com/bootstrap/springboot/util/Converter.java
@@ -1,9 +1,7 @@
 package com.bootstrap.springboot.util;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.modelmapper.ModelMapper;
 
@@ -28,54 +26,4 @@ public class Converter {
 
 		}
 	
-		public Object stringToObject(String mensagemKafka, Object classe) {
-		Class<?> clazz = classe.getClass();
-		
-		Field[] fields = clazz.getFields();
-		List<String> users = Stream.of(mensagemKafka.split(",", -1))
-				  .collect(Collectors.toList());
-		try {
-		for (int i = 0; i < fields.length; i++) {
-			Field fieldID = clazz.getField(fields[i].getName());
-			
-			if (fieldID.getType().getSimpleName().equalsIgnoreCase("Integer"))
-				fieldID.set(classe, new Integer(users.get(i)));
-			if (fieldID.getType().getSimpleName().equalsIgnoreCase("int"))
-				fieldID.set(classe, Integer.parseInt(users.get(i)));
-			if (fieldID.getType().getSimpleName().equalsIgnoreCase("String"))
-				fieldID.set(classe, users.get(i));
-
-		}
-		}catch (NumberFormatException e) {
-			e.printStackTrace();
-}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return classe;
-}
-	
-	public String objectToString(String mensagemKafka, Object classe) {
-		String deserialize = "";
-		Class<?> clazz = classe.getClass();
-		
-		Field[] fields = clazz.getFields();
-		List<String> users = Stream.of(mensagemKafka.split(",", -1))
-				  .collect(Collectors.toList());
-		try {
-		for (int i = 0; i < fields.length; i++) {
-			Field fieldID = clazz.getField(fields[i].getName());
-			
-			deserialize += fieldID.get(classe) + ",";
-		}
-		}catch (NumberFormatException e) {
-			e.printStackTrace();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		return deserialize.substring(0, deserialize.length()-1);
-	}
-	
-	private String gerarPiPullNext() { 			  
-		return Base64.getEncoder().encodeToString(LocalDateTime.now().toString().getBytes());		
-	}
 }
